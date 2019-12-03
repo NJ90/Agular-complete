892385bb4bd5e606115fe1242d1df7b1ad045804
nameejang <55773845+nameejang@users.noreply.github.com>
2019-12-03 11:09:32 +0900

    section01 - section02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles:[`
+  h3 {
+    color: dodgerblue
+  }
+  `]//styles를 선언한 뒤 backticks로 multiple line의 inline css 작성 가능, 마찬가지로 간단한 css일 때만 사용
 })
 export class AppComponent {
-  title = 'Agular-complete';
+  name = 'Agular-complete';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
+import { FormsModule } from '@angular/forms';//to enable the "ngModel directive"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule//to enable the "ngModel directive"
   ],
   providers: [],
   bootstrap: [AppComponent]
